824f9147d021cb90c746a75c97bcaef2468f843b
kai <luanvuforwork@gmail.com>
2025-12-30 14:09:44 +0700

    fix: update LayoutProvider to use overflow-auto for better content visibility

--- components/layout-provider.tsx
+++ components/layout-provider.tsx
@@ -6,14 +6,12 @@ export default function LayoutProvider({
   children: React.ReactNode;
 }) {
   return (
-    <section className="relative flex flex-col overflow-hidden">
+    <section className="relative flex flex-col overflow-auto">
       <div className="bottom-10 right-10 fixed z-50">
         <Header />
       </div>
 
-      <main className="grow w-full max-w-7xl mx-auto p-4  pb-10">
-        {children}
-      </main>
+      <main className="grow w-full max-w-7xl mx-auto p-4  ">{children}</main>
     </section>
   );
 }
